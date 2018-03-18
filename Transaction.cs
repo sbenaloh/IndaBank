69fcad135a81db01aae3f01d3ae8ef725893101a
STEVEN JEFFREY BENALOH <sbenaloh@cs.washington.edu>
2018-03-18 15:24:08 -0700

    Transaction Class defined

---
+++ Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Transaction
+{
+
+    private DateTime date;
+    private Double amount;
+    private String category;    // Storing as String for now, can be more concrete if desirable.
+
+	public Transaction(DateTime date, Double amount, String category)
+	{
+        this.date = date;
+        this.amount = amount;
+        this.category = category;
+	}
+}
