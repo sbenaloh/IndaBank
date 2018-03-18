e6753eaf3f9773d8c855ea9a135269c3989bf487
Steven Benaloh <sbenaloh@outlook.com>
2018-03-18 16:40:55 -0700

    Added Transaction.cs

---
+++ IndaBank/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * This class defines a basic transaction.
+ */
+public class Transaction
+{
+    private DateTime date;
+    private Double amount;
+    private String category;    // Storing as String for now, can be more concrete if desirable.
+
+    public Transaction(DateTime date, Double amount, String category)
+    {
+        this.date = date;
+        this.amount = amount;
+        this.category = category;
+    }
+}
